e72047c40a426ace38ef8c35a482a23b3b3de3a1
jujulili1 <59768244+jujulili1@users.noreply.github.com>
2020-06-19 11:24:07 +0700

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+let input = prompt("tuliskan semuanya");
 String.prototype.capital = function(allWords) {
     return (allWords) ?
         this.split(' ').map(word => word.capital()).join(' ') :
         this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-console.log("mifah faris hidayat".capital(true));
+console.log(input.capital(true));
+alert(input.capital(true));
